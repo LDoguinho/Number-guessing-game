2d4f27deeb032dc19b7873ad9b70b90dab0ecb63
Leo <doguinholeo03@gmail.com>
2025-01-21 12:06:19 -0300

    Add files via upload

---
+++ GuessNumberGame/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+        GameRules.WelcomeMensage();
+        GameRules.Rules();
+        GameRules.Difficultys();
+        GameConf.ChoseDificulty();
+        
+    }
+}
